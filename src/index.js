2d1cedcedd43bc6b9cab2d55639306b806fbc804
Kiselevich Stanislav <kiselevich.stasik@gmail.com>
2022-02-18 22:51:35 +0300

    feat: add full project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = []
+  const map = new Map()
+  
+  for(item of bracketsConfig) {
+    map.set(item[0],item[1])
+  }
+  
+  for(let i = 0; i < str.length; i++){
+    let currentTag = str[i];
+    
+    if(stack[stack.length - 1] !== currentTag){
+      stack.push(map.get(currentTag))
+      continue;
+    }
+
+    if (stack[stack.length - 1] === currentTag){
+      stack.pop()
+    }
+  }
+  if(stack.length == 0){
+    return true
+  }
+  else {
+    return false
+  }
 }
